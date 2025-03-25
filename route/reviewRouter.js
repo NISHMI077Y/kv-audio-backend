c6209ccf6cadfa8e1cc241c965aaa05122b683dd
NISHMI077Y <samindyapremathunga77@gmail.com>
2025-03-25 13:42:28 +0530

    async await implemention

--- route/reviewRouter.js
+++ route/reviewRouter.js
@@ -1,25 +1,28 @@
 import express from "express";
-import { addReview, deleteReview, getReviews } from "../controllers/reviewController.js";
+import { addReview, approvedReview, deleteReview, getReviews } from "../controllers/reviewController.js";
 
 const reviewRouter = express.Router();
 reviewRouter.post("/",addReview)
 reviewRouter.get("/",getReviews)
 reviewRouter.delete(":email",deleteReview)
 
-reviewRouter.get("/:email",
-     (req,res)=>{
-    console.log("This is email route")
-})
+// reviewRouter.get("/:email",
+//      (req,res)=>{
+//     console.log("This is email route")
+// })
 
-reviewRouter.get("/approved",
-(req,res)=>{
-    console.log("this is approved route")
-})
+// reviewRouter.get("/approved",
+// (req,res)=>{
+//     console.log("this is approved route")
+// })
 
-reviewRouter.get("/:email",
-    (res,req)=>{
-        console.log("This is email route")
-    }
-)
+// reviewRouter.get("/:email",
+//     (res,req)=>{
+//         console.log("This is email route")
+//     })
+
+reviewRouter.put("/approve/:email",approvedReview)
+
+
 
 export default reviewRouter;
