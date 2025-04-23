57169f4d5c3a278ed112d0f32e07a21d1c0c2fb8
NISHMI077Y <samindyapremathunga77@gmail.com>
2025-04-23 22:57:48 +0530

    day 21 💖🌟😎

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,22 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
+import nodemailer from "nodemailer"
+import OTP from "../models/otp.js";
 
 dotenv.config();
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "samindyapremathunga77@gmail.com",
+    pass: "kgljcfgotqxpsoeq",
+  }
+})
 
 export function registerUser(req,res){
 
@@ -48,6 +62,7 @@ export function registerUser(req,res){
                             role : user.role,
                             profilePicture : user.profilePicture,
                             phone: user.phone,
+                            emailVerified: user.emailVerified
 
 
                         },
@@ -142,3 +157,141 @@ export async function getAllUsers(req,res){
       res.status(403).json({error: "Unauthorized"});
     }
   }   
+
+  export async function loginWithGoogle(req,res){
+    
+    	//https://www.googleapis.com/oauth2/v3/userinfo
+
+      const accesToken = req.body.accessToken;
+ 	console.log(accesToken);
+ 	try {
+ 		const response = await axios.get(
+ 			"https://www.googleapis.com/oauth2/v3/userinfo",
+ 			{
+ 				headers: {
+ 					Authorization: `Bearer ${accesToken}`,
+ 				},
+ 			}
+ 		);
+ 		console.log(response.data);
+ 		const user = await User.findOne({
+ 			email: response.data.email,
+ 		});
+ 		if (user != null) {
+ 			const token = jwt.sign(
+ 				{
+ 					firstName: user.firstName,
+ 					lastName: user.lastName,
+ 					email: user.email,
+ 					role: user.role,
+ 					profilePicture: user.profilePicture,
+ 					phone: user.phone,
+           emailVerified: true,
+
+ 				},
+ 				process.env.JWT_SECRET
+ 			);
+ 
+ 			res.json({ message: "Login successful", token: token, user: user });
+ 		} else {
+       const newUser = new User({
+         email: response.data.email,
+         password: "123",
+         firstName: response.data.given_name,
+         lastName: response.data.family_name,
+         address: "Not Given",
+         phone: "Not given",
+         profilePicture: response.data.picture,
+         emailVerified: true,
+       });
+       const savedUser = await newUser.save();
+       const token = jwt.sign(
+         {
+         
+           firstName: savedUser.firstName,
+           lastName: savedUser.lastName,
+           email: savedUser.email,
+           role: savedUser.role,
+           profilePicture: savedUser.profilePicture,
+           phone: savedUser.phone,
+         },
+
+         process.env.JWT_SECRET
+        );
+        res.json({ message: "Login successful", token: token, user: savedUser });
+      }
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({ error: "Failed to login with google" });
+    }
+  }
+
+  export async function sendOTP(req,res){
+    
+ if(req.user == null){
+  res.status(403).json({error : "Unauthorized"})
+  return;
+}
+//generate number between 1000 and 9999
+const otp = Math.floor(Math.random()*9000) + 1000;
+//save otp in database
+const newOTP = new OTP({
+  email : req.user.email,
+  otp : otp
+})
+await newOTP.save();
+
+const message = {
+  from : "samindyapremathunga77@gmail.com",
+  to : req.user.email,
+  subject : "Validating OTP",
+  text : "Your otp code is "+otp
+}
+
+
+transport.sendMail(message, (err, info) => {
+  if(err){
+    console.log(err); 
+    res.status(500).json({error : "Failed to send OTP"})    
+  }else{
+    console.log(info)
+    res.json({message : "OTP sent successfully"})
+  }
+
+});
+
+}
+  
+  export async function verifyOTP(req,res){
+    if(req.user == null){
+      res.status(403).json({error : "Unauthorized"})
+      return;
+    }
+    const code = req.body.code;
+  
+    const otp = await OTP.findOne({
+      email : req.user.email,
+      otp : code
+    })
+  
+    if(otp == null){
+      res.status(404).json({error : "Invalid OTP"})
+      return;
+    }else{
+     
+      await OTP.deleteOne({
+        email : req.user.email,
+        otp : code
+      })
+  
+      await User.updateOne({
+        email : req.user.email
+      },{
+        emailVerified : true
+      })
+  
+      res.status(200).json({message : "Email verified successfully"})
+    }
+    
+  }
+  
--- models/user.js
+++ models/user.js
@@ -41,7 +41,13 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : true,
         default : "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?t=st=1742705281~exp=1742708881~hmac=55ad43d0def8e390059c2f59f503cd422829964e002e0b988a23b786918bd2a4&w=826"
-}
+},
+emailVerified : {
+    type : Boolean,
+    required : true,
+    default : false
+},
+
 }); 
 
 const User = mongoose.model("User", userSchema);
